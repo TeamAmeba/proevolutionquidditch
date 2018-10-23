e1ad5241acdbca8f555d5a485929b5b66610ddee
TeamAmeba <43379282+TeamAmeba@users.noreply.github.com>
2018-10-23 16:27:53 +0200

    Intro acabada
    Se han añadido graficos y efectos de ultima generación

--- src/states/intro.js
+++ src/states/intro.js
@@ -1,18 +1,34 @@
+timer = 0;
 ProEvolutionQuidditch.introState = function(game) {
-
+    
 }
 
 ProEvolutionQuidditch.introState.prototype = {
 
     preload: function() {
-        
+        game.load.image('background', 'assets/images/background/background2.png');
+        game.load.image('photo', 'assets/images/referee.png');
+        game.load.image('text', 'assets/images/text/cleangame.png');
     },
 
     create: function() {
-
+        game.add.sprite(0,0, 'background');
+        sprite= game.add.sprite(280,60,'photo');
+        text = game.add.sprite(400,450, 'text');
+        text.anchor.setTo(0.5,0.5);
+        text.scale.setTo(0.5,0.5);
     },
 
     update: function() {
+        timer += game.time.elapsed;
+        if (timer >= 400){
+            timer -= 400;
+            text.visible = !text.visible;
+        }
+
+        if (game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)){
+            game.state.start('levelState');
+        }
 
     }
 }
