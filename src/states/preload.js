75572a6af8b47f65cdc5b633e6914ac976650286
TeamAmeba <43379282+TeamAmeba@users.noreply.github.com>
2018-10-23 16:15:28 +0200

    Preload

--- src/states/preload.js
+++ src/states/preload.js
@@ -5,11 +5,53 @@ ProEvolutionQuidditch.preloadState = function(game) {
 ProEvolutionQuidditch.preloadState.prototype = {
 
     preload: function() {
-        
+        //Text images
+        game.load.image('cleangame','assets/images/text/cleangame.png');
+        game.load.image('loading1','assets/images/text/loading1.png');
+        game.load.image('loading2','assets/images/text/loading2.png');
+        game.load.image('player1','assets/images/text/player1.png');
+        game.load.image('player2','assets/images/text/player2.png');
+        game.load.image('press1','assets/images/text/press1.png');
+        game.load.image('press2','assets/images/text/press2.png');
+        game.load.image('return','assets/images/text/return.png');
+        game.load.image('start','assets/images/text/start.png');
+        game.load.image('title','assets/images/text/title.png');
+        game.load.image('vs','assets/images/text/vs.png');
+        game.load.image('winner','assets/images/text/winner.png');
+
+        //Background images
+        game.load.image('background','assets/images/background/background.png');
+        game.load.image('background2','assets/images/background/background2.png');
+        game.load.image('stadium1','assets/images/background/stadium1.png');
+        game.load.image('stadium2','assets/images/background/stadium2.png');
+        game.load.image('stadium3','assets/images/background/stadium3.png');
+        game.load.image('stadium4','assets/images/background/stadium4.png');
+
+        //Images
+        game.load.image('harry','assets/images/harry.png');
+        game.load.image('draco','assets/images/draco.png');
+        game.load.image('referee','assets/images/referee.png');
+        game.load.image('griffindor','assets/images/griffindor.png');
+        game.load.image('slytherin','assets/images/slytherin.png');
+        game.load.image('snitch','assets/images/snitch.png');
+        game.load.image('trophy','assets/images/trophy.png');
+
+        //Audio
+        game.load.audio('1','assets/audio/1.ogg');
+        game.load.audio('2','assets/audio/2.ogg');
+        game.load.audio('powerUp9','assets/audio/powerUp9.ogg');
+        game.load.audio('you_win','assets/audio/you_win.ogg');
+
+        //Music
+        game.load.audio('bensound-creepy','assets/audio/bensound-creepy.mp3');
+        game.load.audio('bensound-epic','assets/audio/bensound-epic.mp3');
+        game.load.audio('bensound-happyrock','assets/audio/bensound-happyrock.mp3');
+
+        game.state.start('menuState');
     },
 
     create: function() {
-
+        var s = game.add.sprite(100, 0, 'loading1');
     },
 
     update: function() {
