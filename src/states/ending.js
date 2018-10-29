cbfcb4455dc2acf2bb6567da22422fefe82df7f0
TeamAmeba <43379282+TeamAmeba@users.noreply.github.com>
2018-10-29 17:51:48 +0100

    Add files via upload

--- src/states/ending.js
+++ src/states/ending.js
@@ -1,3 +1,4 @@
+var timer = 0;
 ProEvolutionQuidditch.endingState = function(game) {
 
 }
@@ -9,10 +10,32 @@ ProEvolutionQuidditch.endingState.prototype = {
     },
 
     create: function() {
+        game.add.sprite(0,0,'background2');
+        game.add.sprite(180,320,'winner');
+        game.add.sprite(100,100,'griffindor');
 
+
+        harry = game.add.sprite(640,180,'harry');
+        harry.anchor.setTo(0.5, 0.5);
+        harry.scale.setTo(-1, 1)
+
+        game.add.sprite(330,30,'trophy');
+
+        retrn = game.add.sprite(400,430,'return');
+        retrn.anchor.setTo(0.5, 0.5);
+        retrn.scale.setTo(0.5, 0.5);
     },
 
     update: function() {
+        timer += game.time.elapsed;
+        if ( timer >= 400 ) { 
+            timer -= 400;
+            retrn.visible = !retrn.visible;
+        }
 
+        if (game.input.keyboard.isDown(Phaser.Keyboard.S))
+        {
+            game.state.start('menuState');
+        }
     }
 }
